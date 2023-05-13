32ac2b6de567ee01e8315a3bd1df162652929e19
matan avital <matinza.avital@gmail.com>
2023-05-13 21:12:56 +0300

    check if user already exist during registration

--- communication_ltd_server/routes/register.js
+++ communication_ltd_server/routes/register.js
@@ -24,102 +24,106 @@ router.post('/', (req, res, next) => {
   db.query('SELECT * FROM users WHERE email = $1', [email])
     .then((result) => {
       if (result.rows.length > 0) {
-        return res.status(400).json({
-          message: 'User already exists'
-        });
+        throw new Error("'User already exists'")
       }
     })
+    .then(() => {
+      // Validate password complexity
+      const passwordLength = config.passwordLength;
+      const passwordComplexity = config.passwordComplexity;
+      const passwordDictionary = config.passwordDictionary;
 
-  // Validate password complexity
-  const passwordLength = config.passwordLength;
-  const passwordComplexity = config.passwordComplexity;
-  const passwordDictionary = config.passwordDictionary;
+      if (password.length < passwordLength) {
+        return res.status(400).json({
+          message: `Password must be at least ${passwordLength} characters long`
+        });
+      }
 
-  if (password.length < passwordLength) {
-    return res.status(400).json({
-      message: `Password must be at least ${passwordLength} characters long`
-    });
-  }
+      const hasUppercase = /[A-Z]/.test(password);
+      const hasLowercase = /[a-z]/.test(password);
+      const hasNumber = /[0-9]/.test(password);
+      const hasSpecialChar = /[!@#$%^&*]/.test(password);
 
-  const hasUppercase = /[A-Z]/.test(password);
-  const hasLowercase = /[a-z]/.test(password);
-  const hasNumber = /[0-9]/.test(password);
-  const hasSpecialChar = /[!@#$%^&*]/.test(password);
+      if (passwordComplexity.uppercase && !hasUppercase) {
+        return res.status(400).json({
+          message: 'Password must contain at least one uppercase letter'
+        });
+      }
 
-  if (passwordComplexity.uppercase && !hasUppercase) {
-    return res.status(400).json({
-      message: 'Password must contain at least one uppercase letter'
-    });
-  }
+      if (passwordComplexity.lowercase && !hasLowercase) {
+        return res.status(400).json({
+          message: 'Password must contain at least one lowercase letter'
+        });
+      }
 
-  if (passwordComplexity.lowercase && !hasLowercase) {
-    return res.status(400).json({
-      message: 'Password must contain at least one lowercase letter'
-    });
-  }
+      if (passwordComplexity.numbers && !hasNumber) {
+        return res.status(400).json({
+          message: 'Password must contain at least one number'
+        });
+      }
 
-  if (passwordComplexity.numbers && !hasNumber) {
-    return res.status(400).json({
-      message: 'Password must contain at least one number'
-    });
-  }
+      if (passwordComplexity.specialCharacters && !hasSpecialChar) {
+        return res.status(400).json({
+          message: 'Password must contain at least one special character'
+        });
+      }
 
-  if (passwordComplexity.specialCharacters && !hasSpecialChar) {
-    return res.status(400).json({
-      message: 'Password must contain at least one special character'
-    });
-  }
+      if (passwordDictionary.some((word) => password.toLowerCase().includes(word))) {
+        return res.status(400).json({
+          message: 'Password cannot contain commonly used words'
+        });
+      }
+      // Generate salt for password hashing
+      const salt = crypto.randomBytes(16).toString('hex');
 
-  if (passwordDictionary.some((word) => password.toLowerCase().includes(word))) {
-    return res.status(400).json({
-      message: 'Password cannot contain commonly used words'
-    });
-  }
-  // Generate salt for password hashing
-  const salt = crypto.randomBytes(16).toString('hex');
+      // Hash password with HMAC + salt
+      const hmac = crypto.createHmac('sha512', salt);
+      hmac.update(password);
+      const hashedPassword = hmac.digest('hex');
 
-  // Hash password with HMAC + salt
-  const hmac = crypto.createHmac('sha512', salt);
-  hmac.update(password);
-  const hashedPassword = hmac.digest('hex');
-
-  // Store user in database
-  db.query(`INSERT INTO users
+      // Store user in database
+      db.query(`INSERT INTO users
            (first_name,
             last_name,
             email,
             password,
             salt) VALUES
             ($1, $2, $3, $4, $5)`,
-      [firstName, lastName, email, hashedPassword, salt])
-    .then(() => {
-      // Send confirmation email
-      const transporter = nodemailer.createTransport(config.emailTransport);
-      const mailOptions = {
-        from: config.emailFrom,
-        to: email,
-        subject: 'Welcome to our app!',
-        text: 'Thank you for registering.'
-      };
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error('Email error:', error);
-        } else {
-          console.log('Email sent:', info.response);
-        }
-      });
+          [firstName, lastName, email, hashedPassword, salt])
+        .then(() => {
+          // Send confirmation email
+          const transporter = nodemailer.createTransport(config.emailTransport);
+          const mailOptions = {
+            from: config.emailFrom,
+            to: email,
+            subject: 'Welcome to our app!',
+            text: 'Thank you for registering.'
+          };
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              console.error('Email error:', error);
+            } else {
+              console.log('Email sent:', info.response);
+            }
+          });
 
-      // Return success response
-      res.status(200).json({
-        message: 'User registered successfully'
-      });
+          // Return success response
+          res.status(200).json({
+            message: 'User registered successfully'
+          });
+        })
+        .catch((error) => {
+          console.error('Database error:', error);
+          res.status(500).json({
+            message: 'Internal server error'
+          });
+        })
     })
     .catch((error) => {
-      console.error('Database error:', error);
-      res.status(500).json({
-        message: 'Internal server error'
-      });
-    });
+      return res.status(400).json({
+        message: error.message
+      })
+    })
 });
 
 module.exports = router;
