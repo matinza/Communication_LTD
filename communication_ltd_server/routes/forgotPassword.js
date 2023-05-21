3c4889052a392b7221225130805005667dd1752e
matan avital <matinza.avital@gmail.com>
2023-05-21 04:30:31 +0300

    sha-1

--- communication_ltd_server/routes/forgotPassword.js
+++ communication_ltd_server/routes/forgotPassword.js
@@ -21,46 +21,38 @@ router.post('/', (req, res) => {
     .then((result) => {
       const user = result;
       const {
-        id,
         email
       } = user;
 
-      // generate token
-      payload = {
-        userId: id,
-        userEmail: email
-      }
+      // Generate a random value
+      const randomValue = crypto.randomBytes(16).toString('hex');
 
-      generateAccessToken(payload).then((token) => {
-        const now = moment().format('YYYY-MM-DD HH:mm:ss');
-        db.query('UPDATE users SET reset_password_token = $1, reset_password_expires = $2 WHERE email = $3', [token, now, email])
+      // Generate SHA-1 hash of the random value
+      const sha1Hash = crypto.createHash('sha1').update(randomValue).digest('hex');
+
+      const now = moment().format('YYYY-MM-DD HH:mm:ss');
+      db.query('UPDATE users SET reset_password_token = $1, reset_password_expires = $2 WHERE email = $3', [sha1Hash, now, email])
         .catch((error) => {
           console.error('Database error:', error);
         });
 
-        // Send confirmation email
-        const transporter = nodemailer.createTransport(config.emailTransport);
-        const mailOptions = {
-          from: config.emailFrom,
-          to: email,
-          subject: 'Your new current password',
-          text: token
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error('Email error:', error);
-          } else {
-            console.log('Email sent:', info.response);
-          }
-        });
-        // Return success response
-        res.status(200).json({});
-      }).catch((error) => {
-        console.log('error durign token creation => ', error);
-        res.status(500).json({
-          message: 'Internal server error'
-        });
-      })
+      // Send confirmation email
+      const transporter = nodemailer.createTransport(config.emailTransport);
+      const mailOptions = {
+        from: config.emailFrom,
+        to: email,
+        subject: 'Your new current password',
+        text: token
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Email error:', error);
+        } else {
+          console.log('Email sent:', info.response);
+        }
+      });
+      // Return success response
+      res.status(200).json({});
     })
     .catch((error) => {
       console.log(error.message);
